158b718c77fa73d0434f0020d737794e7d89837e
Ivan Latunov <ivan.latunov@chaosgroup.com>
2019-07-18 11:01:51 +0300

    Use alacritty colors

---
+++ colors_alacritty.h
@@ -0,0 +1,29 @@
+static const char *colorname[] = {
+	"#000000",
+	"#d54e53",
+	"#b9ca4a",
+	"#e6c547",
+	"#7aa6da",
+	"#c397d8",
+	"#70c0ba",
+	"#ffffff",
+
+	"#666666",
+	"#ff3334",
+	"#9ec400",
+	"#e7c547",
+	"#7aa6da",
+	"#b77ee0",
+	"#54ced6",
+	"#ffffff",
+
+	[255] = 0, // Not too sure why I need this
+
+    "#eeeeee",
+};
+
+unsigned int defaultfg = 7;
+unsigned int defaultbg = 0;
+static unsigned int defaultcs = 256;
+static unsigned int defaultrcs = 0;
+
